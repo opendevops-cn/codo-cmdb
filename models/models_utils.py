c0bc7df9a47d27efb1139bdfa55805f2e4862be4
dongdongliu <dongdongliu@huanle.com>
2024-10-12 22:17:54 +0800

    fix

--- models/models_utils.py
+++ models/models_utils.py
@@ -170,14 +170,14 @@ def server_task(cloud_name: str, account_id: str, rows: list) -> Tuple[bool, str
                             AssetServerModels.zone: __info.get('zone'),
                             AssetServerModels.state: __info.get('state'),
                             # AssetServerModels.agent_id: agent_id,
-                            AssetServerModels.agent_info: agent_info,
+                            # AssetServerModels.agent_info: agent_info,
                             AssetServerModels.outer_ip: __info.get('outer_ip'),
                             AssetServerModels.inner_ip: inner_ip,
                             AssetServerModels.is_expired: False,  # 改为正常状态
                             AssetServerModels.ext_info: __info  # 存json
                         }
                         if agent_info:
-                            update_data.update(agent_id=agent_id)
+                            update_data[AssetServerModels.agent_info] = agent_info
                         db_session.query(AssetServerModels).filter_by(**filter_map).update(update_data)
                     except Exception as err:
                         logging.error(err)
