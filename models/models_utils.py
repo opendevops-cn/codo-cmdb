9b9aa69d72d6ac7a0cb721307314ea0c6820b26d
dongdongliu <dongdongliu@huanle.com>
2024-10-12 22:07:54 +0800

    fix: 修复同步agent_info可能为空的bug

--- models/models_utils.py
+++ models/models_utils.py
@@ -162,7 +162,7 @@ def server_task(cloud_name: str, account_id: str, rows: list) -> Tuple[bool, str
                     # 更新时更新agent_info
                     agent_info = all_agent_info.get(exist_id[1], {})
                     try:
-                        db_session.query(AssetServerModels).filter_by(**filter_map).update({
+                        update_data = {
                             AssetServerModels.cloud_name: cloud_name,
                             AssetServerModels.account_id: account_id,
                             AssetServerModels.name: __info.get('name'),
@@ -175,7 +175,10 @@ def server_task(cloud_name: str, account_id: str, rows: list) -> Tuple[bool, str
                             AssetServerModels.inner_ip: inner_ip,
                             AssetServerModels.is_expired: False,  # 改为正常状态
                             AssetServerModels.ext_info: __info  # 存json
-                        })
+                        }
+                        if agent_info:
+                            update_data.update(agent_id=agent_id)
+                        db_session.query(AssetServerModels).filter_by(**filter_map).update(update_data)
                     except Exception as err:
                         logging.error(err)
                 else:
