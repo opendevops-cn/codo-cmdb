1a89df8acf59933249655dbe033a49392025e77c
dongdongliu <dongdongliu@huanle.com>
2025-03-20 19:20:16 +0800

    👌 IMPROVE: 配置示例

--- settings.py
+++ settings.py
@@ -71,8 +71,8 @@ KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "")
 # Sync GCP to CMDB
 GCP_SYNC = os.getenv("GCP_SYNC", "no")
 
-# 服务树告警忽略配置
-INGORE_TREE_ALERT_KEYWORDS = os.getenv("IGNORE_TREE_ALERT_ITEMS", "")
+# 服务树告警忽略配置. e.g: "item1,,,item2,,,item3"
+INGORE_TREE_ALERT_KEYWORDS = os.getenv("IGNORE_TREE_ALERT_ITEMS", "tke-,,,node-00,,,as-tke-,,,k8s-")
 
 try:
     from local_settings import *
